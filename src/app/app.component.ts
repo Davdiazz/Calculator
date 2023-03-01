906f9b79d01a0e8104636e0b5457c936878e6897
Ivan Mauricio Diaz <idiaz@conexia.com>
2023-03-01 18:07:02 -0500

    Impl: calculator front

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,22 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
 
-  datasource = [
-    ['1', '2', '3', '4'],
-    ['5', '6', '7', '8'],
-    ['9', '*', '/', '()'],
-    ['=', '+', '-']
-  ]
 
-  result = '';
-
-
-  onClickButton(value: string) {
-    this.result += value;
-  }
 
 }
